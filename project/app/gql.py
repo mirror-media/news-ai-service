f31d989c70057d773689d15509a4665fdab0f695
Dylan Liu <gaespa007648@gmail.com>
2024-04-11 15:36:27 +0800

    feat: update tags and external tags

--- project/app/gql.py
+++ project/app/gql.py
@@ -1,4 +1,5 @@
 from gql.transport.aiohttp import AIOHTTPTransport
+from gql.transport.requests import RequestsHTTPTransport
 from gql import gql, Client
 
 ### 抓取外部文章的gql
@@ -12,6 +13,9 @@ query Externals {{
     partner {{
       id
       name
+    }}
+    tags {{
+      id
     }}
     thumb
     source
@@ -31,6 +35,26 @@ query Tags {{
 }}
 '''
 
+### 更新tags
+gql_update_tags = """
+mutation CreateTags($data: [TagCreateInput!]!) {
+  createTags(data: $data) {
+    name
+  }
+}
+"""
+
+### 更新external
+gql_update_external = """
+mutation UpdateExternal($where: ExternalWhereUniqueInput!, $data: ExternalUpdateInput!) {
+  updateExternal(where: $where, data: $data) {
+    tags {
+      name
+    }
+  }
+}
+"""
+
 def gql_fetch_async(gql_endpoint, gql_string):
     gql_transport = AIOHTTPTransport(url=gql_endpoint)
     gql_client = Client(transport=gql_transport,
@@ -39,8 +63,55 @@ def gql_fetch_async(gql_endpoint, gql_string):
     return json_data
 
 def gql_fetch_sync(gql_endpoint, gql_string):
-    gql_transport = AIOHTTPTransport(url=gql_endpoint)
+    gql_transport = RequestsHTTPTransport(url=gql_endpoint)
     gql_client = Client(transport=gql_transport,
                         fetch_schema_from_transport=True)
     json_data = gql_client.execute(gql(gql_string))
     return json_data
+  
+def gql_update_sync(gql_endpoint, gql_string, gql_variable):
+    gql_transport = RequestsHTTPTransport(url=gql_endpoint)
+    gql_client = Client(transport=gql_transport,
+                        fetch_schema_from_transport=True)
+    json_data = gql_client.execute(gql(gql_string), variable_values=gql_variable)
+    return json_data
+  
+def generate_tags_string(keyword_table):
+    '''
+      generate the updating tags string from keyword_table
+    '''
+    total_keywords = set()
+    for keywords in keyword_table:
+        total_keywords = total_keywords | set(keywords.keys())
+    
+    tags_string = {
+        "data": []
+    }
+    for keyword in total_keywords:
+        tags_string["data"].append({
+          "name": keyword
+        })
+    return tags_string
+
+    
+def generate_external_string(id: str, keywords: list):
+    '''
+      generate the updating externals string
+    '''
+    external_string = {  
+      "where": {
+        "id": id
+      },
+      "data": {
+        "tags": {
+          "set": []
+        }
+      },
+    }
+    for keyword in keywords:
+        external_string["data"]["tags"]["set"].append(
+          {
+            "name": keyword
+          }
+        )
+    return external_string
--- project/worker.py
+++ project/worker.py
@@ -4,6 +4,7 @@ from celery import Celery
 from app.config import load_vectorizer
 from app.document import gen_external_documents
 from app.synchronize import synchronize_tags
+from app.gql import generate_tags_string, generate_external_string, gql_update_sync, gql_update_tags, gql_update_external
 import meilisearch
 
 celery = Celery(__name__)
@@ -20,26 +21,49 @@ def create_task(task_type):
 @celery.task(name="keyword_task")
 def keyword_task(data: dict):
     externals            = data['externals']
+    gql_endpoint         = os.environ['GQL_ENDPOINT']
     MASTER_KEY           = os.environ['MEILISEARCH_KEY']
     MEILISEARCH_ENDPOINT = os.environ['MEILISEARCH_ENDPOINT']
-    client = meilisearch.Client(MEILISEARCH_ENDPOINT, MASTER_KEY)
     
-    ### synchornize tags first
-    print('syncrhonize tags...')
-    synchronize_tags(client, 1000)
-
     ### run keyword extraction algorithm
     print('start keyword extraction...')
     posts = [post['title']+post['content'] for post in externals]
     keyword_table = keyword_extractor.get_keywords(posts)
     print('keyword extraction finished...')
 
-    ### write the data into meilisearch engine
+    ### write the data into meilisearch engine for furthur usage
+    client = meilisearch.Client(MEILISEARCH_ENDPOINT, MASTER_KEY)
     print('start writing documents into meilisearch...')
     docs = gen_external_documents(externals, keyword_table)
     client.index('externals').add_documents(docs)
     print('Write documents into meilisearch finished...')
 
-    ### TODO: write the information back to cms
-
+    ### update tags on cms
+    tags_string = generate_tags_string(keyword_table)
+    try:
+        gql_update_sync(
+            gql_endpoint = gql_endpoint, 
+            gql_string   = gql_update_tags, 
+            gql_variable = tags_string
+        )
+    except:
+        print("Update with some repetition tags")
+    
+    ### update keyword field of externals
+    for idx, external in enumerate(externals):
+        id   = external['id']
+        tags = external['tags']
+        if tags==None or tags==[]:
+            continue  # we don't overwrite the external which already has tags
+        keywords = list(keyword_table[idx].keys())
+        external_string = generate_external_string(id, keywords)
+        try:
+            gql_update_sync(
+                gql_endpoint = gql_endpoint,
+                gql_string = gql_update_external,
+                gql_variable = external_string
+            )
+            print(f"Successfully update tags for external_id={id}")
+        except:
+            print(f'Update tags for external_id={id} failed')
     return True
